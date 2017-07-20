8744501d7d1dc8023f42938c9640ecf2fc0fbb5d
ijabz <ijabz@fastmail.fm>
2017-07-20 14:56:13 +0100

    #210:Add Test

---
+++ srctest/org/jaudiotagger/issues/Issue210Test.java
@@ -0,0 +1,43 @@
+package org.jaudiotagger.issues;
+
+import org.jaudiotagger.AbstractTestCase;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.mp4.Mp4AtomTree;
+import org.jaudiotagger.tag.FieldKey;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+
+/**
+ * Test
+ */
+public class Issue210Test extends AbstractTestCase
+{
+    public void testReadingInvalidGenre() throws Exception
+    {
+        File orig = new File("testdata", "test210.m4a");
+        if (!orig.isFile())
+        {
+            System.err.println("Unable to test file - not available");
+            return;
+        }
+
+        Exception ex=null;
+        try
+        {
+            File testFile = AbstractTestCase.copyAudioToTmp("test210.m4a");
+            AudioFile af = AudioFileIO.read(testFile);
+            assertNotNull(af.getTag());
+            System.out.println(af.getTag());
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            ex=e;
+        }
+        assertNotNull(ex);
+    }
+
+}
