5971d3b6daa03940908e1d48be7a4a58e397e1cd
ijabz <ijabz@fastmail.fm>
2017-09-14 12:30:24 +0100

    Recuded level of message

--- src/org/jaudiotagger/tag/id3/ID3v23Tag.java
+++ src/org/jaudiotagger/tag/id3/ID3v23Tag.java
@@ -631,7 +631,7 @@ public class ID3v23Tag extends AbstractID3v2Tag
             //Found Padding, no more frames
             catch (PaddingException ex)
             {
-                logger.severe(getLoggingFilename() + ":Found padding starting at:" + byteBuffer.position());
+                logger.info(getLoggingFilename() + ":Found padding starting at:" + byteBuffer.position());
                 break;
             }
             //Found Empty Frame, log it - empty frames should not exist
