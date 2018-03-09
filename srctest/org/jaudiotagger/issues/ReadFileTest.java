8d9e42bfffd578abec1e2e1cd5182740caf8aed8
ijabz <ijabz@fastmail.fm>
2018-03-09 12:22:57 +0000

    Issue #250:Multi-value TXXX frames encoded as UTF-16 not read properly

---
+++ srctest/org/jaudiotagger/issues/ReadFileTest.java
@@ -0,0 +1,39 @@
+package org.jaudiotagger.issues;
+
+import org.jaudiotagger.AbstractTestCase;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.tag.FieldKey;
+
+import java.io.File;
+
+/**
+ * Test
+ */
+public class ReadFileTest extends AbstractTestCase
+{
+    public void testReadFile() throws Exception
+    {
+        File orig = new File("testdata", "test530.wav");
+        if (!orig.isFile())
+        {
+            System.err.println("Unable to test file - not available");
+            return;
+        }
+
+        Exception ex=null;
+        try
+        {
+            File testFile = AbstractTestCase.copyAudioToTmp("test530.wav");
+            AudioFile af = AudioFileIO.read(testFile);
+            assertNotNull(af.getTag());
+            System.out.println(af.getTag());
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            ex=e;
+        }
+        assertNull(ex);
+    }
+}
